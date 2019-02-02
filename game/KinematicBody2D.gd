f8008f2065c10533124ab3f16aa8aa7e1c4f8b5f
Darian <dariann6@gmail.com>
2019-02-02 01:30:57 -0800

    Added Prototype Shiba

--- game/KinematicBody2D.gd
+++ game/KinematicBody2D.gd
@@ -4,11 +4,13 @@ extends KinematicBody2D
 # var a = 2
 # var b = "textvar"
 
-var motion = Vector2()
 const UP = Vector2(0, -1)
 const GRAVITY = 20
 const SPEED = 200
 const JUMP_HEIGHT = 400
+
+var motion = Vector2()
+var double_jumped = false
 func _physics_process(delta):
 	motion.y += GRAVITY
 	if Input.is_action_pressed("ui_right"):
@@ -18,8 +20,12 @@ func _physics_process(delta):
 	else:
 		motion.x = 0
 	
-	if is_on_floor():
-		if Input.is_action_just_pressed("ui_up"):
+	if Input.is_action_just_pressed("ui_up"):
+		if is_on_floor():
 			motion.y = -JUMP_HEIGHT
+			double_jumped = false
+		elif not double_jumped:
+			motion.y = -JUMP_HEIGHT
+			double_jumped = true
 	
 	motion = move_and_slide(motion, UP)
