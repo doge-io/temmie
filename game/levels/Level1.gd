2a5b53545899d9e38a1ca0daeb06ef77b4d81652
Darian <dariann6@gmail.com>
2019-02-03 03:07:07 -0800

    Adjusted character sizes

--- game/levels/Level1.gd
+++ game/levels/Level1.gd
@@ -13,8 +13,15 @@ func _ready():
 func _input(event):
 	if event.is_action_pressed("ui_select"):
     # Just an ugly boolean switch :D
-		players[current_player_index].active = false
+		var current_player = players[current_player_index]
+		current_player.active = false
+		
 		current_player_index = 0 if current_player_index >= len(players)-1 else current_player_index + 1
-		players[current_player_index].active = true
+		current_player = players[current_player_index]
+		current_player.active = true
+		current_player.get_node("Camera2D").current = true
 		
-		
+func _process(delta):
+	for player in players:
+		if player.position.y > 1000:
+			get_tree().reload_current_scene()
