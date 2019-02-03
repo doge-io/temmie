2b4dca6338dc71509603dcda6ac6c71cd94602c9
Darian <dariann6@gmail.com>
2019-02-03 03:40:01 -0800

    Added cycling through characters both ways

--- game/levels/Level1.gd
+++ game/levels/Level1.gd
@@ -11,16 +11,25 @@ func _ready():
 	pass
 
 func _input(event):
-	if event.is_action_pressed("ui_select"):
-    # Just an ugly boolean switch :D
-		var current_player = players[current_player_index]
-		current_player.active = false
+	if event.is_action_pressed("ui_switch_left"):
+		switch_character(true)
+	elif event.is_action_pressed("ui_switch_right"):
+		switch_character(false)
 		
-		current_player_index = 0 if current_player_index >= len(players)-1 else current_player_index + 1
-		current_player = players[current_player_index]
-		current_player.active = true
-		current_player.get_node("Camera2D").current = true
 		
+func switch_character(left):
+	var current_player = players[current_player_index]
+	current_player.active = false
+	
+	current_player_index += -1 if left else 1
+	if (current_player_index < 0):
+		current_player_index = len(players)-1
+	elif current_player_index > len(players)-1:
+		current_player_index = 0
+	current_player = players[current_player_index]
+	current_player.active = true
+	current_player.get_node("Camera2D").current = true
+	
 func _process(delta):
 	for player in players:
 		if player.position.y > 1000:
